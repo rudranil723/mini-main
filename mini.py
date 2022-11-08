545fbc12c90e6d07d0d2a581c19baad60bb04b68
Ruddy <rudranilshil@gmail.com>
2022-11-08 14:44:31 +0530

    comments added on the mini.py file and req.txt up

--- mini.py
+++ mini.py
@@ -1,6 +1,7 @@
 # main file of mi_ni 2.0
 
-#import assets.noteapp as noteapp
+# required imports
+
 from logging import warning
 from turtle import *
 from cgitb import text
@@ -9,50 +10,58 @@ from socket import TCP_NODELAY
 import speech_recognition as sr
 import os
 from gtts import gTTS
-# import datetime
-# import random
 import warnings
 from toml import TomlDecodeError
-# import wikipedia
-# import calendar
 from cgi import print_arguments
 from traceback import print_tb
 from datetime import date
+
+# importing sub-files required for the project
 import passwordcheck
 import ear
 import mouth
 
-# text password funtion to check wether the password entered is correct or not
+# fillter warning funtion to ignore all other unnecessary errors
 
 warnings.filterwarnings("ignore")
 
-
 # main funtion
+
+# this mini.py file is the main file from where the project starts
 
 
 def main():
+    # the user is allowed to enter choice only 3 times
     allowences = 3
     print("\n:::::_WELLCOME USER_:::::\n")
     print("please select how you want to run me(mi_ni)\n")
     print("Enter: 1: for text command")
     print("  or   2: for voice command \n")
+
     while allowences > 0:
         c = int(input('\n enter choice: '))
+
         if c == 1:
-            print('\ngoing forward with text commands')
+            print('\nGoing forward with text commands')
             t = input('\n enter password: ')
+            # calling in the passwordcheck.py file to check wether the phrase entered by the user contains the password/ wake-word
             passwordcheck.text_password(t)
             break
+
         if c == 2:
             print('\nGoing forward with voice control password: \n')
             print('please say any of the wake-words\n')
-            # for calling in the voice control funtion py
-            # key = os.system('python assets/speechtotext_ear.py')
-            # voice='okay going forward with voice control'
+
+            # calling the mouth.py
+            # making mini say by calling the mouth.py ,after which the user shall say the password/ wake word phrase
             mouth.minisay(voice='okay going foward with voice control')
+
+            # calling the ear.py file to make mini listen the input by the user
             key = ear.minihear()
+            # sending the password/ wake-word to the passwordcheck file to check the file
             passwordcheck.speech_password(key)
             break
+
         else:
             print('invalid input, try again ')
         allowences -= 1
