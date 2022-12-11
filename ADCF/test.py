e7681949a252bbcc5e729bd86fd0f89ce91a4ad2
Ruddy <rudranilshil@gmail.com>
2022-12-11 18:35:09 +0530

    text change nltk tokenization first applied

--- ADCF/test.py
+++ ADCF/test.py
@@ -29,23 +29,26 @@
 
 # import the existing word and sentence tokenizing
 # libraries
+import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
 
-text = "Natural language processing (NLP) is a field " + \
-    "of computer science, artificial intelligence " + \
-    "and computational linguistics concerned with " + \
-    "the interactions between computers and human " + \
-    "(natural) languages, and, in particular, " + \
-    "concerned with programming computers to " + \
-    "fruitfully process large natural language " + \
-    "corpora. Challenges in natural language " + \
-    "processing frequently involve natural " + \
-    "language understanding, natural language" + \
-    "generation frequently from formal, machine" + \
-    "-readable logical forms), connecting language " + \
-    "and machine perception, managing human-" + \
-    "computer dialog systems, or some combination " + \
-    "thereof."
+nltk.word_tokenize('text')
+text = "rudy is my name"
+# text = "Natural language processing (NLP) is a field " + \
+#     "of computer science, artificial intelligence " + \
+#     "and computational linguistics concerned with " + \
+#     "the interactions between computers and human " + \
+#     "(natural) languages, and, in particular, " + \
+#     "concerned with programming computers to " + \
+#     "fruitfully process large natural language " + \
+#     "corpora. Challenges in natural language " + \
+#     "processing frequently involve natural " + \
+#     "language understanding, natural language" + \
+#     "generation frequently from formal, machine" + \
+#     "-readable logical forms), connecting language " + \
+#     "and machine perception, managing human-" + \
+#     "computer dialog systems, or some combination " + \
+#     "thereof."
 
 print(sent_tokenize(text))
 print(word_tokenize(text))
