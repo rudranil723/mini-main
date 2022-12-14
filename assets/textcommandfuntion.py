489d134f804065050eb23fe36f5cd01c80af83ec
Ruddy <rudranilshil@gmail.com>
2022-12-14 18:01:43 +0530

    stopwatch funtion added but nlp problem still on

--- assets/textcommandfuntion.py
+++ assets/textcommandfuntion.py
@@ -3,7 +3,6 @@ from asyncio import tasks
 import os
 import sys
 import arrayformationusing_nltk
-import keyword_search
 
 
 def tasks(term):
@@ -13,37 +12,52 @@ def tasks(term):
         # print("\n Enter choise: ")
         # here is where i want to apply machine learning so thata the mini can perform multiplke funtuoin withoutr a list
         a = input('\n user : ')
-        b = arrayformationusing_nltk.arrayformationby_nltk(a)
-        print(b)
-        for i in b:
+        # pa refers to the array formed by the entered phase
+        pa = arrayformationusing_nltk.arrayformationby_nltk(a)
+
+        for i in pa:
+
+            # key words  that the user must use in his phase or command to activate calculator funtion
             cal = ['calculation', 'calculator', 'add',
-                   'substract', 'divide', 'multiply']
+                   'substract', 'divide', 'multiply', 'cal']
             if i in cal:
+                # calling the calculator file location assets/text-funtion/calculator.py to perform calculation
                 os.system('python assets/text-funtion/calculator.py')
                 tasks(1)
+                break
+
+            # key words  that the user must use in his phase or command to activate date&time funtion
             dt = ['date', 'time', 'date time', 'clock']
             if i in dt:
                 os.system('python assets/text-funtion/date-timefuntion.py')
                 tasks(1)
+                break
+
+            # key word  that the user must use in his phase or command to activate heart funtion
+            hrt = ['heart']
+            if i in hrt:
+                os.system('python  assets/text-funtion/heart.py')
+                tasks(1)
+                break
+
+            # key word  that the user must use in his phase or command to activate note-pad funtion
+            np = ['notepad', 'list', 'note']
+            if i in np:
+                os.system('python  assets/text-funtion/noteapp.py')
+                tasks(1)
+                break
+
+            # key word  that the user must use in his phase or command to activate stopwatch funtion
+            sw = ['stopwatch', 'timer']
+            if i in sw:
+                os.system('python assets/text-funtion/stopwatch.py')
+
+            # here enter more tasks that the AI must perform
+
+            # final command
+            else:
+                print('sorry mini did not understand your command please retry')
+                tasks(1)
 
 
-        # if a == 1:
-        #     os.system('python assets/text-funtion/calculator.py')
-        #     tasks(1)
-        # if a == 2:
-        #     # this funtion is not working
-        #     os.system('python assets/text-funtion/datetime.py')
-        #     tasks(1)
-        # if a == 3:
-        #     os.system('python  assets/text-funtion/heart.py')
-        #     tasks(1)
-        # if a == 4:
-        #     os.system('python  assets/text-funtion/noteapp.py')
-        #     tasks(1)
-        # if a == 5:
-        #     print('goodbye')
-        # i want to implement restert in the code but dont know how to go out of the current folder and call mini.py
-        # if a == 6:
-        # sys.path.insert(0, '/mini-main/mini.py')
-        # os.system('python ')
 tasks(1)
