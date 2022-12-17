61c68ca6d7bf9179d426664d4337860089c440c3
Ruddy <rudranilshil@gmail.com>
2022-12-18 01:02:18 +0530

    started implementing gn

---
+++ tkinker/impl.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+root = Tk()
+
+
+def main():
+    myLabel = Label(
+        root, text="\n:::::_WELLCOME USER_:::::\n \nplease select how you want to run me(mi_ni)\n \nEnter: 1: for text command\n \n  or   2: for voice command \n", bg='pink', fg='green')
+    myLabel.pack()
+
+
+main()
+root.mainloop()
+# from tkinter import *
+# root = Tk()
+# w = Label(root, text='GeeksForGeeks.org!')
+# w.pack()
+# root.mainloop()
--- tkinker/tkinkerlearn.py
+++ tkinker/tkinkerlearn.py
@@ -6,12 +6,11 @@ e.pack()
 
 
 def myClick():
-    hello = 'hellow'+e.get()
     myLabel = Label(root, text="hellow " + e.get())
     myLabel.pack()
 
 
-myButton = Button(root, text='enter name', command=myClick)
-myButton.pack()
+# myButton = Button(root, text='enter name', command=myClick)
+# myButton.pack()
 
 root.mainloop()
