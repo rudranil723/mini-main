6760d991f6279cf2d332bfd32949692dee00482a
Ruddy <rudranilshil@gmail.com>
2022-12-18 06:06:03 +0530

    tkinter works

--- tkinker/impl.py
+++ tkinker/impl.py
@@ -1,18 +1,31 @@
 from tkinter import *
+import os
 
 root = Tk()
+
+# adding background color to the whole tab
+root.configure(bg="pink")
+
+# adjusting size of the window
+root.geometry("600x600")
+root.minsize(400, 400)
+
+# declaring the main funtion
+
+e = Entry(root)
 
 
 def main():
     myLabel = Label(
-        root, text="\n:::::_WELLCOME USER_:::::\n \nplease select how you want to run me(mi_ni)\n \nEnter: 1: for text command\n \n  or   2: for voice command \n", bg='pink', fg='green')
+        root, text="\n:::::_WELLCOME USER_:::::\n \nplease select how you want to run me(mi_ni)\n \npress button 1 for text command\n \n  or press button 2 for voice command \n", bg='pink', fg='green')
+    myLabel2 = Label(root, text="Enter choise:")
     myLabel.pack()
+    myLabel2.pack()
+    myButton1 = Button(root, text="text command", width=15, height=2)
+    myButton2 = Button(root, text="voice command", width=15, height=2)
+    myButton1.pack()
+    myButton2.pack()
 
 
 main()
 root.mainloop()
-# from tkinter import *
-# root = Tk()
-# w = Label(root, text='GeeksForGeeks.org!')
-# w.pack()
-# root.mainloop()
--- tkinker/tkinkerlearn.py
+++ tkinker/tkinkerlearn.py
@@ -1,16 +1,23 @@
 from tkinter import *
 
 root = Tk()
-e = Entry(root, width=50)
-e.pack()
+# e = Entry(root, width=50)
+# e.pack()
 
 
-def myClick():
-    myLabel = Label(root, text="hellow " + e.get())
-    myLabel.pack()
+# def myClick():
+#     myLabel = Label(root, text="hellow " + e.get())
+#     myLabel.pack()
 
 
-# myButton = Button(root, text='enter name', command=myClick)
-# myButton.pack()
+# # myButton = Button(root, text='enter name', command=myClick)
+# # myButton.pack()
 
+
+b = Button(root, text="Enter", width=10, height=2)
+b.config()
+b.pack(side=LEFT)
+
+c = Button(root, text="Clear", width=10, height=2)
+c.pack(side=LEFT)
 root.mainloop()
