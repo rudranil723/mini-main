f4b0bd7c0a88a7e4bfb367009dc6638f3ec814d8
Ruddy <rudranilshil@gmail.com>
2022-12-19 17:46:44 +0530

    font chnaged

--- tkinker/impl.py
+++ tkinker/impl.py
@@ -17,14 +17,17 @@ e = Entry(root)
 
 def main():
     myLabel = Label(
-        root, text="\n:::::_WELLCOME USER_:::::\n \nplease select how you want to run me(mi_ni)\n \npress button 1 for text command\n \n  or press button 2 for voice command \n", bg='pink', fg='green')
-    myLabel2 = Label(root, text="Enter choise:")
-    myLabel.pack()
-    myLabel2.pack()
-    myButton1 = Button(root, text="text command", width=15, height=2)
-    myButton2 = Button(root, text="voice command", width=15, height=2)
-    myButton1.pack()
-    myButton2.pack()
+        root, text="\n_____WELLCOME USER_____\n \nplease select how you want to run me (mi_ni)\n \npress button 1 for text command\n \n  or press button 2 for voice command \n", bg='pink', fg='purple', font=25)
+    myLabel2 = Label(root, text="Enter choise:", font=20)
+    myLabel.pack(pady=20, padx=20)
+    myLabel2.pack(pady=20, padx=20)
+    myButton1 = Button(root, text="text command", width=15,
+                       height=2, bg='#FFFFE0', fg='blue')
+    myButton2 = Button(root, text="voice command", width=15,
+                       height=2, bg='#FFFFE0', fg='blue')
+    # need to place the buttons side by side idk how
+    myButton1.pack(padx=10)
+    myButton2.pack(pady=20)
 
 
 main()
